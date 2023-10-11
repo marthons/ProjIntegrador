8ec6eaf66c8e5b9f00e02ca6e761826f41f88fae
Guilherme de Souza (hguigit) <guilherme.silva@technosoftware.com.br>
2023-10-11 17:14:24 -0300

    Adicionando home nova

---
+++ Menu/menuScript.js
@@ -0,0 +1,46 @@
+const menuContainer = document.getElementById("home-header");
+
+const menuNav = document.createElement("nav");
+
+const menuItems = [
+    { label: "Top Filmes", link: "topFilmes.html" },
+    { label: "Comentarios", link: "../comentarios-filmes-series.html" },
+    { label: "Destaques de Hoje", link: "../destaquesDeHoje.html" },
+    { label: "Atores", link: "../atores-vida-obra.html" },
+];
+
+const menuList = document.createElement("ul");
+
+menuItems.forEach(item => {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = item.link;
+    link.textContent = item.label;
+    listItem.appendChild(link);
+    menuList.appendChild(listItem);
+});
+
+const imageLogoLink = document.createElement("a");
+imageLogoLink.href = "../Home/home.html"
+
+const imageLogo = document.createElement("span")
+imageLogo.id = "logo-site"
+imageLogo.innerHTML= "IMBD"
+
+imageLogoLink.appendChild(imageLogo);
+
+
+const personLink = document.createElement("a");
+personLink.classList.add('material-icons');
+personLink.classList.add('profile-icon')
+personLink.textContent = 'account_circle'
+personLink.href = "../login.html"
+
+const divMenuProfile = document.createElement("div");
+divMenuProfile.id = "div-menu-profile"
+
+menuContainer.appendChild(menuNav);
+menuNav.appendChild(imageLogoLink)
+divMenuProfile.appendChild(menuList)
+divMenuProfile.appendChild(personLink)
+menuNav.appendChild(divMenuProfile)
