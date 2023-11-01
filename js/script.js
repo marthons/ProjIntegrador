7d6b00ea9f60eb5c861bd0be3f4a136a1e623296
Thon <tonmarchetti.im@gmail.com>
2023-11-01 18:37:58 -0300

    enviar comentario

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
-const controls = document.querySelectorAll(".control");
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
+function adicionarComentario() {
 
-controls.forEach((control) => {
-    control.addEventListener("click", (e) => {
-        isLeft = e.target.classList.contains("arrow-left");
+    let nome = document.getElementById("nome").value;
+    let comentario = document.getElementById("comentario").value;
+    let nota = document.getElementById("nota").value;
 
-        if (isLeft) {
-            currentItem -= 1;
-        } else {
-            currentItem += 1;
-        }
+    // Verificar se os campos não estão vazios
+    if (nome.trim() === "" || comentario.trim() === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;        
+    }
 
-        if (currentItem >= maxItems) {
-            currentItem = 0;
-        }
+    // Criar um elemento para exibir o novo comentário
+    let novoComentario = document.createElement("div");
+    novoComentario.className = "comentario";
 
-        if (currentItem < 0) {
-            currentItem = maxItems - 1;
-        }
+    // Criar elementos para mostrar o nome, comentário e nota
+    let nomeElement = document.createElement("p");
+    nomeElement.className = "nome-comentario";
+    nomeElement.textContent = nome;
 
-        items.forEach((item) => item.classList.remove("current-item"));
+    let comentarioElement = document.createElement("p");
+    comentarioElement.className = "texto-comentario";
+    comentarioElement.textContent = comentario;
 
-        items[currentItem].scrollIntoView({
-            behavior: "smooth",
-            inline: "center"
-        });
+    let notaElement = document.createElement("p");
+    notaElement.className = "nota-comentario";
+    notaElement.textContent = "Avaliação: " + nota;
 
-        items[currentItem].classList.add("current-item");
-    });
-});
+    // Anexar elementos ao novoComentario
+    novoComentario.appendChild(nomeElement);
+    novoComentario.appendChild(comentarioElement);
+    novoComentario.appendChild(notaElement);
+
+    // Anexar o novoComentario ao elemento de comentários existente
+    let comentariosContainer = document.getElementById("comentarios-container");
+    comentariosContainer.appendChild(novoComentario);
+
+    // Limpar os campos do formulário
+    document.getElementById("nome").value = "";
+    document.getElementById("comentario").value = "";
+    document.getElementById("nota").value = "5";
+}
